4e0b2ff34c82b61216d9c0dc7e7c3e863f53a2d5
Arlann <handikaarlan@gmail.com>
2024-11-02 12:17:27 +0700

    feat: add cart page view

--- app/Http/Livewire/Shop/Cartnav.php
+++ app/Http/Livewire/Shop/Cartnav.php
@@ -8,17 +8,22 @@ use Livewire\Component;
 class Cartnav extends Component
 {
     public $cartTotal = 0;
+
     protected $listeners = [
-        'addToCart' => 'updateCartTotal'
+        'addToCart' => 'updateCartTotal',
+        'removeFromCart' => 'updateCartTotal'
     ];
+
     public function mount()
     {
         $this->updateCartTotal();
     }
+
     public function render()
     {
         return view('livewire.shop.cartnav');
     }
+
     public function updateCartTotal()
     {
         $this->cartTotal = count(Cart::get()['products']);
